26d24f809bc31a0d19ae11fc242d553c0d05ef9f
Optim-II <willpearsonphotos@gmail.com>
2018-05-18 17:21:05 -0600

    Added the code to setup the frame.

--- src/tri/view/TriFrame.java
+++ src/tri/view/TriFrame.java
@@ -1,5 +1,28 @@
 package tri.view;
 
-public class TriFrame {
+import tri.controller.TriController;
 
+import javax.swing.JFrame;
+
+public class TriFrame extends JFrame
+{
+	private TriController appController;
+	private TriPanel appPanel;
+	
+	public TriFrame(TriController appController)
+	{
+		super();
+		this.appController = appController;
+		this.appPanel = new TriPanel(appController);
+		
+		setupFrame();
+	}
+	
+	public void setupFrame()
+	{
+		this.setContentPane(appPanel);		
+		this.setTitle("Window title will go here");
+		this.setSize(500, 500);
+		this.setVisible(true);
+	}
 }
