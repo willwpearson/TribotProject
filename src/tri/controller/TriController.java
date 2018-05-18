67e825d54050fd9f79842a0f757a44d6f69ba996
Optim-II <willpearsonphotos@gmail.com>
2018-05-18 17:09:19 -0600

    Created a new project.

---
+++ src/tri/controller/TriController.java
@@ -0,0 +1,5 @@
+package tri.controller;
+
+public class TriController {
+
+}
---
+++ src/tri/controller/TriRunner.java
@@ -0,0 +1,10 @@
+package tri.controller;
+
+public class TriRunner 
+{
+	public static void main(String [] args)
+	{
+		TriController app = new TriController();
+		app.start();
+	}
+}
